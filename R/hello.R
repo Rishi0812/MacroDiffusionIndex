0d54b8cc16531d5bc2d92e13ff04e50ec15678d5
Rishi0812 <rishiramesh08@gmail.com>
2021-06-03 22:29:46 +0530

    Created Base Package

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,6 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+hello <- function(x) {
+  print(paste0("Hello,", x,"!"))
 }
