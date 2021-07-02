342dcf6d952cce16f5c4eb335a2264704b0ce875
Rishi0812 <rishiramesh08@gmail.com>
2021-07-02 15:17:50 +0530

    Added funtion to get available fred data

--- R/mdi_get_data.R
+++ R/mdi_get_data.R
@@ -12,9 +12,64 @@
 #'
 
 mdi_get_data <- function(data){
-  fundamental = getSymbols.FRED('NFCI',env=globalenv())
 
-  behavioral = getSymbols.FRED('UMCSENT',env=globalenv())
+##Fundamental
+
+  #RELATIVE VALUATION
+
+  #ECONOMIC TREND
+  getSymbols.FRED('OECDLOLITOAASTSAM',env=globalenv())
+  getSymbols.FRED('ICSA',env=globalenv())
+  getSymbols.FRED('INDPRO',env=globalenv())
+  getSymbols.FRED('T10Y2Y',env=globalenv())
+  getSymbols.FRED('BAA10Y',env=globalenv())
+
+  #LIQUIDITY
+  getSymbols.FRED('NFCI',env=globalenv())
+
+  #VELOCITY
+  getSymbols.FRED('M2V',env=globalenv())
+  getSymbols.FRED('TOTCI',env=globalenv())
+  getSymbols.FRED('M0263AUSM500NNBR',env=globalenv())
+  getSymbols.FRED('M0264AUSM500NNBR',env=globalenv())
+  getSymbols.FRED('BOGZ1FA895050005Q',env=globalenv())
+
+##BEHAVIORAL
+
+  #CONFIRMATION BIAS: SURVEYS
+  getSymbols.FRED('UMCSENT',env=globalenv())
+  getSymbols.FRED('CSUSHPISA',env=globalenv())
+  getSymbols.FRED('SPCS20RSA',env=globalenv())
+
+  #REPRESNTATIVENESS BIAS: OPTIONS MARKET
+  getSymbols.FRED('VIXCLS',env=globalenv())
+  getSymbols.FRED('VXVCLS',env=globalenv())
+  getSymbols.FRED('EVZCLS',env=globalenv())
+  getSymbols.FRED('THREEFYTP10',env=globalenv())
+
+  #COGNITIVE DISSONANCE
+  getSymbols.FRED('EMVOVERALLEMV',env=globalenv())
+
+  #LOSS AVERSION
+
+  #ANCHORING BIAS
+
+  #FRAMING BIAS
+
+  #OVERCONFIDENCE
+
+
+##CATALYST
+
+  #EARNINGS SURPRISE
+
+  #ECONOMIC SURPRISE
+  getSymbols.FRED('CFNAIMA3',env=globalenv())
+  getSymbols.FRED('STLENI',env=globalenv())
+  getSymbols.FRED('STLFSI2',env=globalenv())
+
+  #GEOPOLITICS
+  getSymbols.FRED('WLEMUINDXD',env=globalenv())
 
 }
 
