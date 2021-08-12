742af3c2c24f528602ad83412d68cd6d87d8bb91
Rishi0812 <rishiramesh08@gmail.com>
2021-08-12 20:29:13 +0530

    Updated/Catagorized Diffusion Indexes

--- R/MDI_Forcast.R
+++ R/MDI_Forcast.R
@@ -2,41 +2,76 @@
 library(quantmod)
 
 #Create new environment
-econ_data <- new.env()
-csv_data <- new.env()
+fundamental_data <- new.env()
+behavioral_data <- new.env()
+catalyst_data <- new.env()
 
 ### Add symbols
-symbols <- c('AMTMNO', 'AMTMTI', 'OECDLOLITOAASTSAM', 'ICSA', 'INDPRO', 'T10Y2Y', 'BAA10Y', 'MKTGDPHKA646NWDB', 'MKTGDPSGA646NWDB', 'RGDPNABRA666NRUG', # Economic Trend
+symbols1 <- c('AMTMNO', 'AMTMTI', 'OECDLOLITOAASTSAM', 'ICSA', 'INDPRO', 'T10Y2Y', 'BAA10Y', 'MKTGDPHKA646NWDB', 'MKTGDPSGA646NWDB', 'RGDPNABRA666NRUG', # Economic Trend
              'NFCI', # Liquidity
-             'M2V', 'TOTCI', 'BOGZ1FA895050005Q', # Velocity
-             'UMCSENT', 'CSUSHPISA', 'SPCS20RSA', # Confirmation Bias: Surveys
-             'VIXCLS', 'VXVCLS', 'EVZCLS', 'THREEFYTP10', # Representative Bias
-             'EMVOVERALLEMV', # Cognitive Dissonance
-             'BOGZ1FA483064105Q', # Overconfidence
-             'CFNAIMA3', 'STLENI', 'STLFSI2', # Economic Surprise
-             'WLEMUINDXD' # Geopolitics
-)
+             'M2V', 'TOTCI', 'BOGZ1FA895050005Q' # Velocity
+             )
 
+symbols2 <- c('UMCSENT', 'CSUSHPISA', 'SPCS20RSA', # Confirmation Bias: Surveys
+              'VIXCLS', 'VXVCLS', 'EVZCLS', 'THREEFYTP10', # Representative Bias
+              'EMVOVERALLEMV', # Cognitive Dissonance
+              'BOGZ1FA483064105Q' # Overconfidence
+              )
+
+symbols3 <- c('CFNAIMA3', 'STLFSI2', # Economic Surprise
+              'WLEMUINDXD' # Geopolitics
+              )
 
 ### Get data
-getSymbols(Symbols = symbols,
+getSymbols(Symbols = symbols1,
            src='FRED',
-           env = econ_data)
+           env = fundamental_data)
 
-rm(symbols)
+getSymbols(Symbols = symbols2,
+           src='FRED',
+           env = behavioral_data)
 
+getSymbols(Symbols = symbols3,
+           src='FRED',
+           env = catalyst_data)
+
+rm(symbols1)
+rm(symbols2)
+rm(symbols3)
 
 ## Get CSV Data
 
+fundamental_csv <- new.env()
+behavioral_csv <- new.env()
+catalyst_csv <- new.env()
+
+#fundamental
 csvfiles <- list.files(path = "~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data")
 for(i in 1:length(csvfiles)) {
   temp = read.csv(paste0("~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data/",
                       csvfiles[i]))
-  assign(x = paste0('data', i), value = temp, envir = csv_data)
+  assign(x = paste0('data', i), value = temp, envir = fundamental_csv)
 }
 
+#behavioral
+csvfiles <- list.files(path = "~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data")
+for(i in 1:length(csvfiles)) {
+  temp = read.csv(paste0("~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data/",
+                         csvfiles[i]))
+  assign(x = paste0('data', i), value = temp, envir = behavioral_csv)
+}
 
+#catalyst
+csvfiles <- list.files(path = "~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data")
+for(i in 1:length(csvfiles)) {
+  temp = read.csv(paste0("~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data/",
+                         csvfiles[i]))
+  assign(x = paste0('data', i), value = temp, envir = catalyst_csv)
+}
+
+#Data pre-processing
 data2 <- eapply(env = csv_data, FUN = merge.zoo)
+data_2 <- do.call(merge.zoo, data2)
 
 data_fin0 <- lapply(data2, strftime(strptime(data2$Date,"%m/%d/%Y"),"%Y-%m-%d"))
 # dat <- read.csv("~/Documents/Rishi/GSoC_2021/Bloomberg-Dataset/GSOC_macro_Bloomberg_data/GSOC_20dayPutCall.csv")
@@ -50,11 +85,16 @@ data_fin0 <- lapply(data2, strftime(strptime(data2$Date,"%m/%d/%Y"),"%Y-%m-%d"))
 #
 # rm(dat,dat2)
 
+data2$Date<- strftime(strptime(data2$Date,"%m/%d/%Y"),"%Y-%m-%d")
+PutCall <- xts(data2[,2], as.Date(dat2[,1], format = "%Y-%m-%d", env = econ_data))
 
+#as.Date(data2$Date, format="%m-%d-%Y")
+#function(x) as.Date(x[,1], format="%m-%d-%Y")
 
 ### Merge data - this will store it in a list
-data <- eapply(env = econ_data, FUN = merge.xts)
-
+data_fundamental <- eapply(env = fundamental_data, FUN = merge.xts)
+data_behavioral  <- eapply(env = behavioral_data, FUN = merge.xts)
+data_catalyst    <- eapply(env = catalyst_data, FUN = merge.xts)
 
 ## Check Periodicities
 periodicities <- as.data.frame(do.call(rbind, lapply(data, periodicity)))
@@ -65,15 +105,40 @@ periodicities$end <- as.Date(sapply(ls_periodicities, "[[", 4))
 periodicities # view periodicities
 rm(ls_periodicities)
 
+#Convert into quaterly
+data_fin_1 <- lapply(data_fundamental, to.quarterly, OHLC=FALSE)
+data_fin_2 <- lapply(data_behavioral, to.quarterly, OHLC=FALSE)
+data_fin_3 <- lapply(data_catalyst, to.quarterly, OHLC=FALSE)
 
-data_fin <- lapply(data, to.quarterly, OHLC=FALSE)
-data_fin[["STLENI"]] <- NULL
+#clean-up
+rm(data_fundamental)
+rm(data_behavioral)
+rm(data_catalyst)
 
-xts_data_quart <- do.call(merge.xts, data_fin)
-rm(data_fin)
-idx_complete <- which(complete.cases(xts_data_quart) == TRUE)
-trim_data = xts_data_quart[idx_complete]
-rm(xts_data_quart)
+#merge data
+xts_data_quart_1 <- do.call(merge.xts, data_fin_1)
+xts_data_quart_2 <- do.call(merge.xts, data_fin_2)
+xts_data_quart_3 <- do.call(merge.xts, data_fin_3)
+
+#clean-up
+rm(data_fin_1)
+rm(data_fin_2)
+rm(data_fin_3)
+
+#Include complete data
+idx_complete_1 <- which(complete.cases(xts_data_quart_1) == TRUE)
+idx_complete_2 <- which(complete.cases(xts_data_quart_2) == TRUE)
+idx_complete_3 <- which(complete.cases(xts_data_quart_3) == TRUE)
+
+#Final trimed Datasets
+trim_data_fundamental = xts_data_quart_1[idx_complete_1]
+trim_data_behavioral = xts_data_quart_2[idx_complete_2]
+trim_data_catalyst = xts_data_quart_3[idx_complete_3]
+
+#clean-up
+rm(xts_data_quart_1)
+rm(xts_data_quart_2)
+rm(xts_data_quart_3)
 
 
 #xts_data_quart = xts_data_quart["1974-12-01/2020-12-01"]
@@ -89,114 +154,7 @@ results <- SWfore(y = trim_data$UMCSENT, x = trim_data[,-c(1)], orig = length(id
 # results <- SWfore(y = trim_data$UMCSENT, x = trim_data[,-1], orig = length(idx_complete)-1, m = 5)
 cbind(trim_data$UMCSENT[(length(idx_complete)-2+1):length(idx_complete)], results$yhat)
 
-
-## SWfore without look-ahead bias
-#UMCSENT
-results <- SWfore(y = trim_data$UMCSENT[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$UMCSENT)-3, m = 5)
-UMCSENT_Forecast <- cbind(trim_data$UMCSENT[(length(trim_data$UMCSENT)-2+1):length(trim_data$UMCSENT)], results$yhat)
-
-#EVZCLS
-results <- SWfore(y = trim_data$EVZCLS[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$EVZCLS)-3, m = 5)
-EVZCLS_Forecast <- cbind(trim_data$EVZCLS[(length(trim_data$EVZCLS)-2+1):length(trim_data$EVZCLS)], results$yhat)
-
-#OECDLOLITOAASTSAM
-results <- SWfore(y = trim_data$OECDLOLITOAASTSAM[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$OECDLOLITOAASTSAM)-3, m = 5)
-OECDLOLITOAASTSAM_Forecast <- cbind(trim_data$OECDLOLITOAASTSAM[(length(trim_data$OECDLOLITOAASTSAM)-2+1):length(trim_data$OECDLOLITOAASTSAM)], results$yhat)
-
-#SPCS20RSA
-results <- SWfore(y = trim_data$SPCS20RSA[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$SPCS20RSA)-3, m = 5)
-SPCS20RSA_Forecast <- cbind(trim_data$SPCS20RSA[(length(trim_data$SPCS20RSA)-2+1):length(trim_data$SPCS20RSA)], results$yhat)
-
-#CFNAIMA3
-results <- SWfore(y = trim_data$CFNAIMA3[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_dataCFNAIMA3)-3, m = 5)
-CFNAIMA3_Forecast <- cbind(trim_data$CFNAIMA3[(length(trim_data$CFNAIMA3)-2+1):length(trim_data$CFNAIMA3)], results$yhat)
-
-#THREEFYTP10
-results <- SWfore(y = trim_data$THREEFYTP10[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$THREEFYTP10)-3, m = 5)
-THREEFYTP10_Forecast <- cbind(trim_data$THREEFYTP10[(length(trim_data$THREEFYTP10)-2+1):length(trim_data$THREEFYTP10)], results$yhat)
-
-#MKTGDPHKA646NWDB
-results <- SWfore(y = trim_data$MKTGDPHKA646NWDB[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$MKTGDPHKA646NWDB)-3, m = 5)
-MKTGDPHKA646NWDB_Forecast <- cbind(trim_data$MKTGDPHKA646NWDB[(length(trim_data$MKTGDPHKA646NWDB)-2+1):length(trim_data$MKTGDPHKA646NWDB)], results$yhat)
-
-#VIXCLS
-results <- SWfore(y = trim_data$VIXCLS[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$VIXCLS)-3, m = 5)
-VIXCLS_Forecast <- cbind(trim_data$VIXCLS[(length(trim_data$VIXCLS)-2+1):length(trim_data$VIXCLS)], results$yhat)
-
-#BAA10Y
-results <- SWfore(y = trim_data$BAA10Y[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$BAA10Y)-3, m = 5)
-BAA10Y_Forecast <- cbind(trim_data$BAA10Y[(length(trim_data$BAA10Y)-2+1):length(trim_data$BAA10Y)], results$yhat)
-
-#VXVCLS
-results <- SWfore(y = trim_data$VXVCLS[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$VXVCLS)-3, m = 5)
-VXVCLS_Forecast <- cbind(trim_data$VXVCLS[(length(trim_data$VXVCLS)-2+1):length(trim_data$VXVCLS)], results$yhat)
-
-#BOGZ1FA483064105Q
-results <- SWfore(y = trim_data$BOGZ1FA483064105Q[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$BOGZ1FA483064105Q)-3, m = 5)
-BOGZ1FA483064105Q_Forecast <- cbind(trim_data$BOGZ1FA483064105Q[(length(trim_data$BOGZ1FA483064105Q)-2+1):length(trim_data$BOGZ1FA483064105Q)], results$yhat)
-
-#MKTGDPSGA646NWDB
-results <- SWfore(y = trim_data$MKTGDPSGA646NWDB[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$MKTGDPSGA646NWDB)-3, m = 5)
-MKTGDPSGA646NWDB_Forecast <- cbind(trim_data$MKTGDPSGA646NWDB[(length(trim_data$MKTGDPSGA646NWDB)-2+1):length(trim_data$MKTGDPSGA646NWDB)], results$yhat)
-
-#EMVOVERALLEMV
-results <- SWfore(y = trim_data$EMVOVERALLEMV[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$EMVOVERALLEMV)-3, m = 5)
-EMVOVERALLEMV_Forecast <- cbind(trim_data$EMVOVERALLEMV[(length(trim_data$EMVOVERALLEMV)-2+1):length(trim_data$EMVOVERALLEMV)], results$yhat)
-
-#AMTMTI
-results <- SWfore(y = trim_data$AMTMTI[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$AMTMTI)-3, m = 5)
-AMTMTI_Forecast <- cbind(trim_data$AMTMTI[(length(trim_data$AMTMTI)-2+1):length(trim_data$AMTMTI)], results$yhat)
-
-#T10Y2Y
-results <- SWfore(y = trim_data$T10Y2Y[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$T10Y2Y)-3, m = 5)
-T10Y2Y_Forecast <- cbind(trim_data$T10Y2Y[(length(trim_data$T10Y2Y)-2+1):length(trim_data$T10Y2Y)], results$yhat)
-
-#WLEMUINDXD
-results <- SWfore(y = trim_data$WLEMUINDXD[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$WLEMUINDXD)-3, m = 5)
-WLEMUINDXD_Forecast <- cbind(trim_data$WLEMUINDXD[(length(trim_data$WLEMUINDXD)-2+1):length(trim_data$WLEMUINDXD)], results$yhat)
-
-#NFCI
-results <- SWfore(y = trim_data$NFCI[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$NFCI)-3, m = 5)
-NFCI_Forecast <- cbind(trim_data$NFCI[(length(trim_data$NFCI)-2+1):length(trim_data$NFCI)], results$yhat)
-
-#STLFSI2
-results <- SWfore(y = trim_data$STLFSI2[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$STLFSI2)-3, m = 5)
-STLFSI2_Forecast <- cbind(trim_data$STLFSI2[(length(trim_data$STLFSI2)-2+1):length(trim_data$STLFSI2)], results$yhat)
-
-#AMTMNO
-results <- SWfore(y = trim_data$AMTMNO[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$AMTMNO)-3, m = 5)
-AMTMNO_Forecast <- cbind(trim_data$AMTMNO[(length(trim_data$AMTMNO)-2+1):length(trim_data$AMTMNO)], results$yhat)
-
-#RGDPNABRA666NRUG
-results <- SWfore(y = trim_data$RGDPNABRA666NRUG[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$RGDPNABRA666NRUG)-3, m = 5)
-RGDPNABRA666NRUG_Forecast <- cbind(trim_data$RGDPNABRA666NRUG[(length(trim_data$RGDPNABRA666NRUG)-2+1):length(trim_data$RGDPNABRA666NRUG)], results$yhat)
-
-#CSUSHPISA
-results <- SWfore(y = trim_data$CSUSHPISA[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$CSUSHPISA)-3, m = 5)
-CSUSHPISA_Forecast <- cbind(trim_data$CSUSHPISA[(length(trim_data$CSUSHPISA)-2+1):length(trim_data$CSUSHPISA)], results$yhat)
-
-#TOTCI
-results <- SWfore(y = trim_data$TOTCI[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$TOTCI)-3, m = 5)
-TOTCI_Forecast <- cbind(trim_data$TOTCI[(length(trim_data$TOTCI)-2+1):length(trim_data$TOTCI)], results$yhat)
-
-#ICSA
-results <- SWfore(y = trim_data$ICSA[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$ICSA)-3, m = 5)
-ICSA_Forecast <- cbind(trim_data$ICSA[(length(trim_data$ICSA)-2+1):length(trim_data$ICSA)], results$yhat)
-
-#M2V
-results <- SWfore(y = trim_data$M2V[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$M2V)-3, m = 5)
-M2V_Forecast <- cbind(trim_data$M2V[(length(trim_data$M2V)-2+1):length(trim_data$M2V)], results$yhat)
-
-#BOGZ1FA895050005Q
-results <- SWfore(y = trim_data$BOGZ1FA895050005Q[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$BOGZ1FA895050005Q)-3, m = 5)
-BOGZ1FA895050005Q_Forecast <- cbind(trim_data$BOGZ1FA895050005Q[(length(trim_data$BOGZ1FA895050005Q)-2+1):length(trim_data$BOGZ1FA895050005Q)], results$yhat)
-
-#INDPRO
-results <- SWfore(y = trim_data$INDPRO[-1], x = lag(trim_data[,-c(1)], 1)[-1], orig = length(trim_data$INDPRO)-3, m = 5)
-INDPRO_Forecast <- cbind(trim_data$INDPRO[(length(trim_data$INDPRO)-2+1):length(trim_data$INDPRO)], results$yhat)
-
-
-## Using SWFore to build Diffusion Indexes
+## Using SWFore to build Diffusion Indexes - Function
 "DiffIdx" <- function(x,orig,m){
   ### Builds Stock and Watson's diffusion index prediction
   ### x: observed regressors
@@ -244,9 +202,25 @@ INDPRO_Forecast <- cbind(trim_data$INDPRO[(length(trim_data$INDPRO)-2+1):length(
   DiffIdx <- DF
 }
 
-diffIdx <- DiffIdx(trim_data[,-c(1)], orig = length(idx_complete)-2, m = 5)
-head(diffIdx)
+#Constructing Diffusion Index
 
+#fundamental
+fundamental_DI <- DiffIdx(trim_data_fundamental, orig = length(idx_complete_1)-2, m = 1)
+head(fundamental_DI)
+
+#behavioral
+behavioral_DI <- DiffIdx(trim_data_behavioral, orig = length(idx_complete_2)-2, m = 1)
+head(behavioral_DI)
+
+#catalyst
+catalyst_DI <- DiffIdx(trim_data_catalyst, orig = length(idx_complete_3)-2, m = 1)
+head(catalyst_DI)
+
+
+## Getting S&P 500 Data
+SP_500 <- getSymbols (Symbols = 'SP500', src= 'FRED', auto.assign = FALSE)
+SP_500_quart <- to.quarterly(SP_500, )
+SP_500_quart <- lapply (SP_500, to.quarterly, OHLC=FALSE)
 
 ## Using Random Forest
 
